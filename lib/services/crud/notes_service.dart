08be571cfbe49b375425af6353b961e85bae9659
akbarjon <akbar.rashidov.2013@mai.ru>
2022-05-11 20:23:09 +0300

    step-15_fixedDbBug

--- lib/services/crud/notes_service.dart
+++ lib/services/crud/notes_service.dart
@@ -309,7 +309,7 @@ class DatabaseNote {
   int get hashCode => id.hashCode;
 }
 
-const dbName = 'notes.db';
+const dbName = 'note.db';
 const noteTable = 'note';
 const userTable = 'user';
 const idColumn = 'id';
@@ -318,15 +318,15 @@ const userIdColumn = 'user_id';
 const textColumn = 'text';
 const isSyncedWithCloudColumn = 'is_synced_with_cloud';
 const createUserTable = '''CREATE TABLE IF NOT EXISTS "user" (
-	      "id"	INTEGER NOT NULL,
-	      "email"	TEXT NOT NULL UNIQUE,
-	      PRIMARY KEY("id" AUTOINCREMENT)
-    );''';
+          "id"	INTEGER NOT NULL,
+          "email"	TEXT NOT NULL UNIQUE,
+          PRIMARY KEY("id" AUTOINCREMENT)
+      );''';
 const createNoteTable = '''CREATE TABLE IF NOT EXISTS "note" (
-      "id"	INTEGER NOT NULL,
-      "user_id"	INTEGER NOT NULL,
-      "text"	TEXT,
-      "is_synced_with_cloud"	INTEGER NOT NULL DEFAULT 0,
-      FOREIGN KEY("user_id") REFERENCES "user"("id"),
-      PRIMARY KEY("id" AUTOINCREMENT)
-    );''';
+        "id"	INTEGER NOT NULL,
+        "user_id"	INTEGER NOT NULL,
+        "text"	TEXT,
+        "is_synced_with_cloud"	INTEGER NOT NULL DEFAULT 0,
+        FOREIGN KEY("user_id") REFERENCES "user"("id"),
+        PRIMARY KEY("id" AUTOINCREMENT)
+      );''';
